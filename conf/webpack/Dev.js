3e68785fe7d2dbac0cf39ce6a06ae47c85609f1d
Owen Corpening <ocorpening@gmail.com>
2017-02-28 15:27:45 -0600

    added consle log

--- conf/webpack/Dev.js
+++ conf/webpack/Dev.js
@@ -11,10 +11,12 @@ class WebpackDevConfig extends WebpackBaseConfig {
   constructor() {
     super();
     const portNum = parseInt(process.env.OPENSHIFT_NODEJS_PORT || 8080);
+    const hostName = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+    console.log("%%%%%%^^^^^^ OPENSHIFT_NODEJS_IP: " + process.env.OPENSHIFT_NODEJS_IP);
     this.config = {
       devtool: 'cheap-module-source-map',
       entry: [
-        'webpack-dev-server/client?http://0.0.0.0:' + portNum,
+        'webpack-dev-server/client?http://' + hostName + ':' + portNum,
         'webpack/hot/only-dev-server',
         'react-hot-loader/patch',
         './client.js'
